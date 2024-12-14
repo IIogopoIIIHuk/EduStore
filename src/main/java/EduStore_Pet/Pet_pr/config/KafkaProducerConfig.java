78afa1e974d2b7b832d7677ca6ba7d8bf9574de5
IIogopoIIIHuk <Sasha.mamchits19@gmail.com>
2024-12-14 16:13:16 +0300

    config kafka producer

---
+++ src/main/java/EduStore_Pet/Pet_pr/config/KafkaProducerConfig.java
@@ -0,0 +1,36 @@
+package EduStore_Pet.Pet_pr.config;
+
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory(){
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress); // в этом свойстве указываются адреса брокеров Kafka,
+                                                                        // которые представляют собой список пар хост-порт, разделенных запятыми.
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class); // определяет как будет сериализован ключ сообщения перед отправкой в Kafka
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class); // то же самое, что и с ключом, только со значением
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+}
